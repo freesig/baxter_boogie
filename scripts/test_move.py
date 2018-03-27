2f5c864fbd01f24166cea967f72cd5220cf33b50
Jake <s3448342@student.rmit.edu.au>
2018-03-27 17:34:10 +1100

    test move updated

--- scripts/test_move.py
+++ scripts/test_move.py
@@ -3,53 +3,94 @@
 import rospy
 from std_msgs.msg import String
 
+import time
+
 from geometry_msgs.msg import Twist
 
-from beginner_tutorials.msg import music_keys
+#from beginner_tutorials.msg import music_keys
 
-base_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+import socket
+IP = "10.42.1.254"
+UDP_IP = "127.0.0.1"
+UDP_PORT = 65000
+
+#base_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
+base_pub = rospy.Publisher('/safebase/cmd_vel', Twist, queue_size=1)
 direction = 1
 beat_waiting = False;
 
 import thread
 import time
 
+DEFAULT_SPEED = 0.0
+DEFAULT_SPIN = 1.0
+
+beat_num = 0
+
 def beat(timer, num):
     global direction
     global beat_waiting
+    global beat_num
     beat_waiting = True;
     time.sleep(timer);
-    direction = direction * -1;
+    if (beat_num % 2 == 0):
+        direction = direction * -1;
+    beat_num += 1
     beat_waiting = False;
-
-def send_move(publisher, music_keys):
-    msg = Twist()
-    msg.linear.x = music_keys.volume*direction;
-    msg.angular.z = music_keys.volume;
-    publisher.publish(msg);
-    if not beat_waiting:
-        thread.start_new_thread(beat, (music_keys.beat, 0));
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard vol: %s", data.volume)
     send_move(base_pub, data);
+
+def recv_data(data, publisher):
+    data = float(data);
+    print (time.time());
+    print (data);
+    time_until = data - time.time();
+    print time_until;
+    if not beat_waiting:
+        thread.start_new_thread(beat, (DEFAULT_SPEED, 0));
+
+def do_publish(publisher, nil):
+    hz = 10;
+    frame_time_ms = 1000 / hz;
+    duration_ms = 20000; # ms
+    elapsed = 0;
+    rate = rospy.Rate(hz) # 10hz
+    while not rospy.is_shutdown() and elapsed < duration_ms:
+        msg = Twist()
+        msg.linear.x = DEFAULT_SPEED;
+        print msg.linear.x;
+        msg.angular.z = DEFAULT_SPIN*direction;
+        publisher.publish(msg);
+        elapsed += frame_time_ms;
+        rate.sleep()
 
 def main():
 
     hz = 10;
 
     rospy.init_node('test', anonymous=True)
-    rospy.Subscriber("chatter", music_keys, callback)
+    #rospy.Subscriber("chatter", music_keys, callback)
     rate = rospy.Rate(hz) # 10hz
 
     frame_time_ms = 1000 / hz;
-    duration_ms = 5000; # ms
+    duration_ms = 2000; # ms
     elapsed = 0;
 
-    while not rospy.is_shutdown() and elapsed < duration_ms:
-        elapsed += frame_time_ms;
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((IP, UDP_PORT))
+
+    thread.start_new_thread(do_publish, (base_pub, 0));
+
+    while True:
+        print "waiting"
+        data, addr = sock.recvfrom(1024);
+        recv_data(data, base_pub);
+
+
+        #rate.sleep()
         #send_move(base_pub, [1.0, 1.0]);
-        rate.sleep()
 
     rospy.spin()
 
