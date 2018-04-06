468ad48a52470b558000da4721124f0cfc28f775
Jake <s3448342@student.rmit.edu.au>
2018-04-06 13:36:33 +1000

    robot responds to energy

--- scripts/test_move.py
+++ scripts/test_move.py
@@ -23,7 +23,9 @@ import thread
 import time
 
 DEFAULT_SPEED = 0.0
-DEFAULT_SPIN = 1.0
+DEFAULT_SPIN = 0.3
+
+spin_speed = DEFAULT_SPEED;
 
 beat_num = 0
 
@@ -33,35 +35,53 @@ def beat(timer, num):
     global beat_num
     beat_waiting = True;
     time.sleep(timer);
-    if (beat_num % 2 == 0):
-        direction = direction * -1;
-    beat_num += 1
-    beat_waiting = False;
+    #if (beat_num % 2 == 0):
+    direction = direction * -1;
+    #beat_num += 1
+    #beat_waiting = False;
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard vol: %s", data.volume)
     send_move(base_pub, data);
 
 def recv_data(data, publisher):
-    data = float(data);
+
+    global spin_speed;
+
+    list_data = data.split(',');
+
+    print list_data[1];
+    list_data[1] = float(list_data[1]);
+
+    list_data[1] = min(3000, list_data[1]);
+    list_data[1] /= 3000;
+
+    #list_data[1] *= 0.5;
+
+    spin_speed = list_data[1];
+
+    list_data[0] = float(list_data[0]);
     print (time.time());
-    print (data);
-    time_until = data - time.time();
-    print time_until;
-    if not beat_waiting:
-        thread.start_new_thread(beat, (DEFAULT_SPEED, 0));
+    #print (data);
+    time_until = list_data[0] - time.time();
+    #print time_until;
+    #if not beat_waiting:
+    thread.start_new_thread(beat, (DEFAULT_SPEED, 0));
 
 def do_publish(publisher, nil):
     hz = 10;
     frame_time_ms = 1000 / hz;
-    duration_ms = 20000; # ms
+    duration_ms = 30000; # ms
     elapsed = 0;
     rate = rospy.Rate(hz) # 10hz
     while not rospy.is_shutdown() and elapsed < duration_ms:
         msg = Twist()
         msg.linear.x = DEFAULT_SPEED;
-        print msg.linear.x;
-        msg.angular.z = DEFAULT_SPIN*direction;
+        #print msg.linear.x;
+
+        #print ("spin speed", spin_speed);
+
+        msg.angular.z = spin_speed*direction;
         publisher.publish(msg);
         elapsed += frame_time_ms;
         rate.sleep()
