68392bbe98b4673f2d41996ac37f3681b8a12028
rmit-s3448342-Jake-Williams <s3448342@student.rmit.edu.au>
2018-04-19 21:49:58 +1000

    increments using vector class

---
+++ scripts/move_left_inc.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013-2015, Rethink Robotics
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the Rethink Robotics nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Baxter RSDK Inverse Kinematics Example
+"""
+
+import thread
+import threading
+
+import time
+
+import argparse
+import struct
+import sys
+
+import rospy
+
+from geometry_msgs.msg import (
+        PoseStamped,
+        Pose,
+        Point,
+        Quaternion,
+        )
+from std_msgs.msg import Header
+
+from baxter_core_msgs.srv import (
+        SolvePositionIK,
+        SolvePositionIKRequest,
+        )
+
+import baxter_interface
+import baxter_external_devices
+
+from baxter_interface import CHECK_VERSION
+
+import socket
+IP = "10.42.1.254"
+UDP_IP = "127.0.0.1"
+UDP_PORT = 55000
+
+SPEED = 1.0;
+
+import Vectors
+init_pos = Vectors.V4D(0.65, 0.05, 0.53, 0);
+bound = Vectors.V4D(0.65, 0.55, 0.53, 0);
+quaternion = Vectors.V4D(0.36, 0.88, -0.10, 0.26);
+
+DEFAULT_INCREMENT = Vectors.V4D(0.00, 0.05, 0.00, 0);
+
+current_pos = init_pos;
+
+def left_arm():
+    position = Pose(
+            position=Point(
+                x=current_pos.x(),
+                y=current_pos.y(),
+                z=current_pos.z(),
+                ),
+            orientation=Quaternion(
+                x=quaternion.x(),
+                y=quaternion.y(),
+                z=quaternion.z(),
+                w=quaternion.w(),
+                ),
+            )
+
+    ik_test('left', position)
+    time.sleep(1);
+
+def ik_test(limb, pose):
+    rospy.init_node("move_left")
+    ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
+    iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+    ikreq = SolvePositionIKRequest()
+    hdr = Header(stamp=rospy.Time.now(), frame_id='base')
+    ps = PoseStamped(
+            header=hdr,
+            pose=pose,
+            )
+
+    ikreq.pose_stamp.append(ps)
+    try:
+        rospy.wait_for_service(ns, 5.0)
+        resp = iksvc(ikreq)
+    except (rospy.ServiceException, rospy.ROSException), e:
+        rospy.logerr("Service call failed: %s" % (e,))
+        return 1
+
+    # Check if result valid, and type of seed ultimately used to get solution
+    # convert rospy's string representation of uint8[]'s to int's
+    resp_seeds = struct.unpack('<%dB' % len(resp.result_type),
+            resp.result_type)
+    if (resp_seeds[0] != resp.RESULT_INVALID):
+        seed_str = {
+                ikreq.SEED_USER: 'User Provided Seed',
+                ikreq.SEED_CURRENT: 'Current Joint Angles',
+                ikreq.SEED_NS_MAP: 'Nullspace Setpoints',
+                }.get(resp_seeds[0], 'None')
+        print("SUCCESS - Valid Joint Solution Found from Seed Type: %s" %
+                (seed_str,))
+        # Format solution into Limb API-compatible dictionary
+        limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
+        print "\nIK Joint Solution:\n", limb_joints
+        print "------------------"
+        print "Response Message:\n", resp
+
+        arm = baxter_interface.Limb(limb)
+        lj = arm.joint_names()
+
+        #current_position = left.joint_angle(lj[3])
+        #joint_command = {lj[3]: current_position - delta}
+
+        command = {};
+
+        for i in range(0, len(resp.joints[0].name)):
+            command[resp.joints[0].name[i]] = resp.joints[0].position[i];
+
+        print command;
+
+        arm.move_to_joint_positions(command)
+
+        global SPEED;
+
+        arm.set_joint_position_speed(SPEED);
+
+    else:
+        print("INVALID POSE - No Valid Joint Solution Found.")
+
+    return 0
+
+def decide_increment(data): # todo - decide based on data
+    return DEFAULT_INCREMENT;
+
+def main():
+    """RSDK Inverse Kinematics Example
+
+    A simple example of using the Rethink Inverse Kinematics
+    Service which returns the joint angles and validity for
+    a requested Cartesian Pose.
+
+    Run this example, passing the *limb* to test, and the
+    example will call the Service with a sample Cartesian
+    Pose, pre-defined in the example code, printing the
+    response of whether a valid joint solution was found,
+    and if so, the corresponding joint angles.
+    """
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((IP, UDP_PORT))
+
+    lim = 10;
+    j = 0;
+
+    global SPEED;
+    global current_pos;
+
+    while (j < lim):
+        j += 1;
+        print "waiting at ", j
+        data, addr = sock.recvfrom(1024);
+        mdata = recv_data(data, 1);
+
+        ct = time.time()
+
+        while (float(ct) < float(mdata[0])):
+
+            ct = time.time();
+            time.sleep(0.01);
+
+        SPEED = mdata[1];
+
+        increment = decide_increment(mdata); 
+
+        test_pos = current_pos + increment;
+
+        if (test_pos > bound): # true if x, y, and z (not w) in test_pos are all bigger than in bound
+            continue;               # this > is some hacky bullshit, temporary safety measure
+        else: current_pos = test_pos; 
+
+        left_arm();
+
+        # increment should have a constant magnitude,
+        # however on which axes it operates and which direction
+        # can be determined by musical cues in 'decide_increment'
+
+
+
+def recv_data(data, p):
+
+    list_data = data.split(',');
+
+    print list_data[1];
+    list_data[1] = float(list_data[1]);
+
+    list_data[1] = min(3000, list_data[1]);
+    list_data[1] /= 3000;
+    list_data[0] = float(list_data[0]);
+
+    return list_data;
+
+if __name__ == '__main__':
+    sys.exit(main())
